2738b3b25aeb5f275fbcf8c8e961828723044137
raj580like-ops <raj580like@gmail.com>
2025-09-13 09:59:11 +0530

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,18 @@
+// js/firebase-config.js
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDI9uNRs7e7T-oX1MYdBEcTJy-XYQhHorw",
+  authDomain: "health-basket-1603c.firebaseapp.com",
+  projectId: "health-basket-1603c",
+  storageBucket: "health-basket-1603c.firebasestorage.app",
+  messagingSenderId: "820149827368",
+  appId: "1:820149827368:web:05e08a6b463a12bb73421d"
+};
+
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
